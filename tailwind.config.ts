ac94dbcc30be5d40c3f22ad6350ca595077bfd93
Dreadhalor <Scotthetrick2@yahoo.com>
2023-06-14 16:54:54 -0700

    Added some colors to Tailwind Added instructions to npm link library in docs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,28 @@ import type { Config } from 'tailwindcss';
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        wool: {
+          dark: '#FBF9F4',
+          DEFAULT: '#FFF8E6',
+          light: '#FFFDF8',
+        },
+        ash: {
+          DEFAULT: '#221E20',
+          medium: '#747474',
+          light: '#D1D1D1',
+          lightest: '#F9F9F9',
+        },
+        gold: '#EEA820',
+        beryl: '#3FCFAD',
+        mosaic: '#24CAFF',
+        heart: '#A52F76',
+        ribbon: '#008FBD',
+        success: '#20A585',
+        danger: '#B71717',
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
