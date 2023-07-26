3ea096adc502d925cc1890dfce5a51a2c7229738
Scott Hetrick <scotthetrick2@yahoo.com>
2023-07-25 20:50:30 -0700

    Improved GithubCard example aesthetics

--- src/examples/components/GithubCard.tsx
+++ src/examples/components/GithubCard.tsx
@@ -1,10 +1,10 @@
+import { useState } from 'react';
 import {
   ChevronDownIcon,
   CircleIcon,
   PlusIcon,
   StarIcon,
 } from '@radix-ui/react-icons';
-
 import {
   Button,
   Card,
@@ -23,6 +23,9 @@ import {
 } from '@src/index';
 
 export function GithubCardDemo(_) {
+  const [futureIdeasChecked, setFutureIdeasChecked] = useState(false);
+  const [myStackChecked, setMyStackChecked] = useState(false);
+  const [inspirationChecked, setInspirationChecked] = useState(true);
   return (
     <Card>
       <CardHeader className='~grid ~grid-cols-[1fr_110px] ~items-start ~gap-4 ~space-y-0'>
@@ -33,15 +36,18 @@ export function GithubCardDemo(_) {
             Tailwind CSS.
           </CardDescription>
         </div>
-        <div className='~flex ~items-center ~space-x-1 ~rounded-md ~bg-secondary ~text-secondary-foreground'>
-          <Button variant='secondary' className='~flex-1 ~px-3 ~shadow-none'>
+        <div className='~flex ~items-center ~rounded-md ~bg-secondary ~text-secondary-foreground'>
+          <Button
+            variant='secondary'
+            className='~flex-1 ~px-3 ~font-medium ~normal-case ~shadow-none'
+          >
             <StarIcon className='~mr-2 ~h-4 ~w-4' />
             Star
           </Button>
           <Separator orientation='vertical' className='~h-[20px] ~bg-input' />
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant='secondary' className='~px-2 ~shadow-none'>
+              <Button variant='secondary' className='~px-3 ~shadow-none'>
                 <ChevronDownIcon className='~h-4 ~w-4 ~text-secondary-foreground' />
               </Button>
             </DropdownMenuTrigger>
@@ -53,11 +59,24 @@ export function GithubCardDemo(_) {
             >
               <DropdownMenuLabel>Suggested Lists</DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuCheckboxItem checked>
+              <DropdownMenuCheckboxItem
+                checked={futureIdeasChecked}
+                onCheckedChange={setFutureIdeasChecked}
+              >
                 Future Ideas
               </DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>My Stack</DropdownMenuCheckboxItem>
-              <DropdownMenuCheckboxItem>Inspiration</DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={myStackChecked}
+                onCheckedChange={setMyStackChecked}
+              >
+                My Stack
+              </DropdownMenuCheckboxItem>
+              <DropdownMenuCheckboxItem
+                checked={inspirationChecked}
+                onCheckedChange={setInspirationChecked}
+              >
+                Inspiration
+              </DropdownMenuCheckboxItem>
               <DropdownMenuSeparator />
               <DropdownMenuItem>
                 <PlusIcon className='~mr-2 ~h-4 ~w-4' /> Create List
