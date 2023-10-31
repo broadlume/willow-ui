c7eece40f9dd2ee729dd540b0aab795c664e2582
Scott Hetrick <scotthetrick2@yahoo.com>
2023-10-30 17:45:25 -0700

    Fixed a lot, committing to switch workstations

--- src/reports/summary-tile/summary-tile.tsx
+++ src/reports/summary-tile/summary-tile.tsx
@@ -3,29 +3,108 @@ import { Skeleton } from '@components/skeleton/skeleton';
 import { cn } from '@src/lib/utils';
 import { FaArrowCircleDown, FaArrowCircleUp } from 'react-icons/fa';
 
-export type SummaryTileProps = {
-  number: number | string;
-  label: string | JSX.Element;
-  delta: string;
+// Component to display the main number
+const NumberDisplay = ({ loading, numberString }) =>
+  loading ? (
+    <Skeleton className='~h-6 ~w-24 ~rounded-md' />
+  ) : (
+    <h5>{numberString}</h5>
+  );
+
+// Component to display the label
+const LabelDisplay = ({ loading, label }) =>
+  loading ? (
+    <Skeleton className='~h-4 ~w-28 ~rounded-md' />
+  ) : (
+    <div className='~flex ~items-center ~gap-2'>{label}</div>
+  );
+
+// Component to display the delta value with arrow
+const DeltaDisplay = ({ loading, Arrow, deltaString, deltaClass, stacked }) => (
+  <div
+    className={`~flex ~items-center ~whitespace-nowrap ~pt-2 ~text-[10px] ${deltaClass}`}
+  >
+    {loading ? (
+      <Skeleton className='~h-4 ~w-20' />
+    ) : deltaString ? (
+      // Show the arrow only if there is a delta value.
+      <>
+        <Arrow className='~text-xs' />
+        &nbsp;{deltaString}&nbsp;
+        <span className='~text-card-foreground'>Change</span>
+      </>
+    ) : (
+      // Place the invisble arrow as a spacer if there is no delta value.
+      // If stacked, don't put the spacer.
+      !stacked && <Arrow className='~invisible ~text-xs' />
+    )}
+  </div>
+);
+
+type DefaultNumProps = {
+  current?: number;
+  previous?: number;
+};
+type CustomNumProps = {
+  number?: number | string;
+  delta?: string;
   negative?: boolean;
+};
+export type SummaryTileNumProps = DefaultNumProps &
+  CustomNumProps & {
+    label?: string | JSX.Element;
+    negativeIsGood?: boolean;
+  };
+
+export type SummaryTileProps = SummaryTileNumProps & {
+  loading?: boolean;
   className?: string;
   stacked?: boolean;
-  loading?: boolean;
 };
 
 const SummaryTile = ({
+  current,
+  previous,
   number,
   label,
   delta,
   negative,
+  negativeIsGood = false,
+  stacked = false,
+  loading = false,
   className,
-  stacked,
-  loading,
 }: SummaryTileProps) => {
-  const deltaClass = negative ? '~text-danger' : '~text-beryl';
-  const Arrow = negative ? FaArrowCircleDown : FaArrowCircleUp;
-  const numberString =
-    typeof number === 'number' ? number.toLocaleString('en-US') : number;
+  // Determine whether to use custom or default formatting.
+  const noCustomFormatting = current !== undefined && previous !== undefined;
+
+  // Function to get the number as a string with formatting.
+  const getNumberString = () => {
+    if (noCustomFormatting) return current.toLocaleString('en-US');
+    return (
+      current?.toLocaleString('en-US') ||
+      (typeof number === 'number' ? number.toLocaleString('en-US') : number)
+    );
+  };
+
+  // Function to calculate and get the delta string.
+  const getDeltaString = () => {
+    if (noCustomFormatting) {
+      const delta = ((current - previous) / previous) * 100;
+      return `${Math.round(delta).toLocaleString('en-US')}%`;
+    }
+    return delta;
+  };
+
+  // Function to determine if the number is negative.
+  const isNegative = noCustomFormatting ? current < previous : negative;
+
+  // Function to determine if the delta is good or not.
+  const isDeltaGood = isNegative ? !negativeIsGood : negativeIsGood;
+
+  // Classes and components based on conditions.
+  const deltaClass = isDeltaGood ? '~text-danger' : '~text-beryl';
+  const Arrow = isNegative ? FaArrowCircleDown : FaArrowCircleUp;
+
   return (
     <Card
       className={cn(
@@ -40,36 +119,23 @@ const SummaryTile = ({
       >
         <div className='~py-auto ~flex ~flex-1 ~flex-col ~items-center ~justify-center'>
           {!stacked && <div className='~flex-1'></div>} {/* spacer */}
-          {loading ? (
-            <Skeleton className='~h-6 ~w-24 ~rounded-md' />
-          ) : (
-            <h5>{numberString}</h5>
-          )}
+          <NumberDisplay loading={loading} numberString={getNumberString()} />
           <p
             className={`caption-2 ~pt-1 ~text-center ~font-normal ~leading-5 ${
               stacked ? '' : '~flex-1'
             }`}
           >
-            {loading ? (
-              <Skeleton className='~h-4 ~w-28 ~rounded-md' />
-            ) : (
-              <div className='~flex ~items-center ~gap-2'>{label}</div>
-            )}
+            <LabelDisplay loading={loading} label={label} />
           </p>
         </div>
-        <div
-          className={`~flex ~items-center ~whitespace-nowrap ~pt-2 ~text-[10px] ${deltaClass}`}
-        >
-          {loading ? (
-            <Skeleton className='~h-4 ~w-20' />
-          ) : (
-            <>
-              <Arrow className='~text-xs' />
-              &nbsp;{delta}&nbsp;
-              <span className='~text-card-foreground'>Change</span>
-            </>
-          )}
-        </div>
+
+        <DeltaDisplay
+          loading={loading}
+          Arrow={Arrow}
+          deltaString={getDeltaString()}
+          deltaClass={deltaClass}
+          stacked={stacked}
+        />
       </CardContent>
     </Card>
   );
