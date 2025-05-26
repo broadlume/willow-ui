80ab832a29d42590ed1737554132b5fa2207898c
Deep Chudasama <deep.chudasama@broadlume.com>
2025-05-26 11:20:52 +0000

    fix(dialog): update DialogContent to accept overlayClassName prop for custom styling

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -42,35 +42,40 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
     onClose?: () => void;
+    overlayClassName?: string;
   }
->(({ className, children, onClose = () => null, ...props }, ref) => (
-  <DialogPortal>
-    <DialogOverlay />
-    <DialogPrimitive.Content
-      ref={ref}
-      className={cn(
-        'tw-reset ~fixed ~left-[50%] ~top-[50%] ~z-50 ~grid ~w-full ~max-w-lg ~translate-x-[-50%] ~translate-y-[-50%] ~gap-4 ~border ~bg-background ~p-6 ~shadow-lg ~duration-200 sm:~rounded-lg md:~w-full',
-        'data-[state=open]:~animate-in data-[state=open]:~fade-in-0 data-[state=open]:~zoom-in-95 data-[state=open]:~slide-in-from-left-1/2 data-[state=open]:~slide-in-from-top-[48%]',
-        'data-[state=closed]:~animate-out data-[state=closed]:~fade-out-0 data-[state=closed]:~zoom-out-95 data-[state=closed]:~slide-out-to-left-1/2 data-[state=closed]:~slide-out-to-top-[48%]',
-        className
-      )}
-      {...props}
-    >
-      {children}
-      <DialogPrimitive.Close
+>(
+  (
+    { className, overlayClassName, children, onClose = () => null, ...props },
+    ref
+  ) => (
+    <DialogPortal>
+      <DialogOverlay className={overlayClassName} />
+      <DialogPrimitive.Content
+        ref={ref}
         className={cn(
-          '~absolute ~right-4 ~top-4 ~rounded-sm ~opacity-70 ~transition-opacity hover:~opacity-100 disabled:~pointer-events-none',
-          '~ring-offset-background focus:~outline-none focus:~ring-2 focus:~ring-ring focus:~ring-offset-2',
-          'data-[state=open]:~bg-accent data-[state=open]:~text-muted-foreground'
+          'tw-reset ~fixed ~left-[50%] ~top-[50%] ~z-50 ~grid ~w-full ~max-w-lg ~translate-x-[-50%] ~translate-y-[-50%] ~gap-4 ~border ~bg-background ~p-6 ~shadow-lg ~duration-200 sm:~rounded-lg md:~w-full',
+          'data-[state=open]:~animate-in data-[state=open]:~fade-in-0 data-[state=open]:~zoom-in-95 data-[state=open]:~slide-in-from-left-1/2 data-[state=open]:~slide-in-from-top-[48%]',
+          'data-[state=closed]:~animate-out data-[state=closed]:~fade-out-0 data-[state=closed]:~zoom-out-95 data-[state=closed]:~slide-out-to-left-1/2 data-[state=closed]:~slide-out-to-top-[48%]',
+          className
         )}
-        onClick={onClose}
+        {...props}
       >
-        <Cross2Icon className='~h-4 ~w-4' />
-        <span className='~sr-only'>Close</span>
-      </DialogPrimitive.Close>
-    </DialogPrimitive.Content>
-  </DialogPortal>
-));
+        {children}
+        <DialogPrimitive.Close
+          className={cn(
+            '~absolute ~right-4 ~top-4 ~rounded-sm ~opacity-70 ~transition-opacity hover:~opacity-100 disabled:~pointer-events-none',
+            '~ring-offset-background focus:~outline-none focus:~ring-2 focus:~ring-ring focus:~ring-offset-2',
+            'data-[state=open]:~bg-accent data-[state=open]:~text-muted-foreground'
+          )}
+          onClick={onClose}
+        >
+          <Cross2Icon className='~h-4 ~w-4' />
+          <span className='~sr-only'>Close</span>
+        </DialogPrimitive.Close>
+      </DialogPrimitive.Content>
+    </DialogPortal>
+  ));
 DialogContent.displayName = DialogPrimitive.Content.displayName;
 
 const DialogHeader = ({
