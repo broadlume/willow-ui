039e5469d20c43d30a8d2a277678efb6401eafac
Shiva Kumar Godasu <46907165+skgodasu9@users.noreply.github.com>
2025-07-25 12:46:03 -0500

    refactor: use generic types

--- src/components/lazy-loaded-selector/lazy-loaded-selector.tsx
+++ src/components/lazy-loaded-selector/lazy-loaded-selector.tsx
@@ -4,43 +4,41 @@ import { HiStar, HiOutlineStar, HiOutlineIdentification, HiMiniIdentification } 
 import { Command, CommandInput, CommandList } from '@components/command/command';
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@components/tooltip/tooltip';
 
-type Client = {
+type Items = {
   id: string;
   name: string;
-  subdomain: string;
+  url: string;
   tenantId: string;
-  active: boolean;
-  status: number;
 };
 
-interface ClientPickerProps {
-  clients: Client[];
+interface LazyLoadedSelectorProps<T> {
+  items: T[];
   onScroll: () => void;
   onSearch: (q: string) => void;
-  onSelect: (client: Client) => void;
+  onSelect: (item: T) => void;
   wrapClassName?: string;
-  selectedClient?: Client | null;
   isCms?: boolean
   placeholderText?: string;
+  storageKey?: string;
 }
 
-const ClientPicker: React.FC<ClientPickerProps> = ({
-  clients,
+const LazyLoadedSelector = <T extends Items>({
+  items,
   onScroll,
   onSearch,
   onSelect,
   wrapClassName = '',
-  selectedClient,
   isCms,
-  placeholderText = 'Search website'
-}: ClientPickerProps) => {
+  placeholderText = 'Search website',
+  storageKey = 'favoriteClients',
+}: LazyLoadedSelectorProps<T>) => {
   const [isTooltipOpen, setIsTooltipOpen] = useState(false);
   const [activeTooltipId, setActiveTooltipId] = useState<string | null>(null);
-  const [favoriteClients, setFavoriteClients] = useState<string[]>(JSON.parse(localStorage.getItem('favoriteClients') || '[]'));
+  const [favoriteItems, setFavoriteItems] = useState<string[]>(JSON.parse(localStorage.getItem(storageKey) || '[]'));
 
   const handleScrollDownEvent = async (e: React.UIEvent<HTMLDivElement>) => {
     const target = e.currentTarget;
-    if (clients && clients.length > 0) {
+    if (items && items?.length > 0) {
       if (
         Math.abs(target.scrollHeight - target.scrollTop - target.clientHeight) < 5
       ) {
@@ -49,8 +47,8 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
     }
   };
 
-  const handleSelect = (client: Client) => {
-    onSelect(client);
+  const handleSelect = (item: T) => {
+    onSelect(item);
   };
 
   const handleCopy = (text: string) => {
@@ -59,12 +57,12 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
     });
   };
 
-  const handleFavorites = (clientId: string) => {
-    const updatedFavorites = favoriteClients.includes(clientId)
-      ? favoriteClients.filter(id => id !== clientId)
-      : [...favoriteClients, clientId];
-    localStorage.setItem('favoriteClients', JSON.stringify(updatedFavorites));
-    setFavoriteClients(updatedFavorites);
+  const handleFavorites = (itemId: string) => {
+    const updatedFavorites = favoriteItems.includes(itemId)
+      ? favoriteItems.filter(id => id !== itemId)
+      : [...favoriteItems, itemId];
+    localStorage.setItem(storageKey, JSON.stringify(updatedFavorites));
+    setFavoriteItems(updatedFavorites);
   };
 
   return (
@@ -76,30 +74,30 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
         data-testid="website-search"
       />
       <CommandList onScroll={handleScrollDownEvent} className='~max-w-[400px]'>
-        {clients?.length > 0 ? (
+        {items?.length > 0 ? (
           <div
             data-testid="website-list"
             id="lazyDropdownSearch-listbox"
             className="~px-2 ~py-1.5"
           >
-            {clients?.map((clientItem: Client, index: number) => {
+            {items?.map((item: T, index: number) => {
               return (
                 <div
-                  data-testid={'website-item-' + clientItem?.name}
+                  data-testid={'website-item-' + item?.name}
                   className="~grow ~px-2 ~py-1.5
                     ~rounded-sm ~flex"
-                  key={clientItem?.id}
+                  key={item?.id}
                 >
-                  <div className='~flex-grow' onClick={() => handleSelect(clientItem)}>
+                  <div className='~flex-grow' onClick={() => handleSelect(item)}>
                     <p data-testid="website-item">
-                      {clientItem?.name}
+                      {item?.name}
                     </p>
-                    {isCms && <a href={clientItem?.subdomain}
+                    {isCms && <a href={item?.url}
                       target="_blank"
                       className=' ~inline-block ~mb-2 ~text-xs ~text-zinc-500'
                       data-testid="website-item-data"
                     >
-                      {clientItem?.subdomain}
+                      {item?.url}
                     </a>}
                   </div>
                   <div className='~flex ~items-start'>
@@ -107,23 +105,23 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
                       <Tooltip
                         onOpenChange={(open) => {
                           setIsTooltipOpen(open);
-                          setActiveTooltipId(open ? clientItem.id : null);
+                          setActiveTooltipId(open ? item.id : null);
                         }}
                       >
                         <TooltipTrigger>
-                          {(isTooltipOpen && activeTooltipId === clientItem.id)
+                          {(isTooltipOpen && activeTooltipId === item.id)
                             ? <HiMiniIdentification className="~h-5 ~w-5" color="#6038E8"/>
                             : <HiOutlineIdentification className="~h-5 ~w-5"/>
                           }
                         </TooltipTrigger>
                         <TooltipContent className='~bg-[#1A1A1A] ~px-3 ~py-2'>
                           {
-                            clientItem?.id && <div className='~flex ~gap-4 ~w-auto'>
+                            item?.id && <div className='~flex ~gap-4 ~w-auto'>
                               <p>
                                 <span className='~font-bold ~text-sm ~text-white'>UUID:</span>
-                                <span className='~text-sm ~text-white'>{" "}{clientItem?.id}</span>
+                                <span className='~text-sm ~text-white'>{" "}{item?.id}</span>
                               </p>
-                              <span onClick={() => handleCopy(clientItem?.id)} className='~cursor-pointer'>
+                              <span onClick={() => handleCopy(item?.id)} className='~cursor-pointer'>
                                 <svg width="16" height="18" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                   <path d="M2.75 1.6875C2.75 1.16973 3.16973 0.75 3.6875 0.75H3.875C4.91053 0.75 5.75 1.58947 5.75 2.625V3.5625C5.75 4.08027 6.16973 4.5 6.6875 4.5H7.625C8.66053 4.5 9.5 5.33947 9.5 6.375V8.0625C9.5 8.58027 9.08027 9 8.5625 9H3.6875C3.16973 9 2.75 8.58027 2.75 8.0625V1.6875Z" fill="#F2F2F2"/>
                                   <path d="M6.5 2.625C6.5 1.96847 6.25898 1.36824 5.8606 0.907952C7.56006 1.35189 8.89811 2.68994 9.34205 4.3894C8.88176 3.99102 8.28153 3.75 7.625 3.75H6.6875C6.58395 3.75 6.5 3.66605 6.5 3.5625V2.625Z" fill="#F2F2F2"/>
@@ -134,12 +132,12 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
                           }
                           {
                             isCms && (
-                              clientItem?.tenantId && <div className='~flex ~gap-4 ~w-auto'>
+                              item?.tenantId && <div className='~flex ~gap-4 ~w-auto'>
                                 <p>
                                   <span className='~font-bold ~text-sm ~text-white'>Site Id:</span>
-                                  <span className='~text-sm ~text-white'>{" "}{clientItem?.tenantId}</span>
+                                  <span className='~text-sm ~text-white'>{" "}{item?.tenantId}</span>
                                 </p>
-                                <span onClick={() => handleCopy(clientItem?.tenantId)} className='~cursor-pointer'>
+                                <span onClick={() => handleCopy(item?.tenantId)} className='~cursor-pointer'>
                                   <svg width="16" height="18" viewBox="0 0 10 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M2.75 1.6875C2.75 1.16973 3.16973 0.75 3.6875 0.75H3.875C4.91053 0.75 5.75 1.58947 5.75 2.625V3.5625C5.75 4.08027 6.16973 4.5 6.6875 4.5H7.625C8.66053 4.5 9.5 5.33947 9.5 6.375V8.0625C9.5 8.58027 9.08027 9 8.5625 9H3.6875C3.16973 9 2.75 8.58027 2.75 8.0625V1.6875Z" fill="#F2F2F2"/>
                                     <path d="M6.5 2.625C6.5 1.96847 6.25898 1.36824 5.8606 0.907952C7.56006 1.35189 8.89811 2.68994 9.34205 4.3894C8.88176 3.99102 8.28153 3.75 7.625 3.75H6.6875C6.58395 3.75 6.5 3.66605 6.5 3.5625V2.625Z" fill="#F2F2F2"/>
@@ -152,8 +150,8 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
                         </TooltipContent>
                       </Tooltip>
                     </TooltipProvider>
-                    {!isCms && <div onClick={() => handleFavorites(clientItem?.id)} className="~ml-2 ~cursor-pointer">
-                      {favoriteClients.includes(clientItem?.id)
+                    {!isCms && <div onClick={() => handleFavorites(item?.id)} className="~ml-2 ~cursor-pointer">
+                      {favoriteItems.includes(item?.id)
                         ? <HiStar className="~h-5 ~w-5" color="#6038E8" />
                         : <HiOutlineStar className="~h-5 ~w-5"/>
                       }
@@ -175,4 +173,4 @@ const ClientPicker: React.FC<ClientPickerProps> = ({
   );
 };
 
-export { ClientPicker}
+export { LazyLoadedSelector }
