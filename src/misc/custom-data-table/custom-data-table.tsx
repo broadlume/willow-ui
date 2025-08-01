a49c2f0dbf70b13500d2ec23e7f8d2ec0aff4255
Dhaval Javia <dhaval.javia@broadlume.com>
2025-08-01 07:04:46 +0000

    fix: shadow -> shadow-sm

--- src/misc/custom-data-table/custom-data-table.tsx
+++ src/misc/custom-data-table/custom-data-table.tsx
@@ -22,7 +22,7 @@ export const CustomDataTable = <T,>({
 
   return (
     <div
-      className={`body-medium tw-reset ~rounded-lg ~border ~shadow ${classNames.root}`}
+      className={`body-medium tw-reset ~rounded-lg ~border ~shadow-sm${classNames.root}`}
     >
       {props.selectableRows ? (
         <DataTable
