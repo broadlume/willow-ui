7ccc88ed73fe532ea58984b6c0131c578f3f0ddb
Dhaval Javia <dhaval.javia@broadlume.com>
2025-08-01 07:06:39 +0000

    fix: rounded-sm -> rounded-xs

--- src/components/dialog/dialog.tsx
+++ src/components/dialog/dialog.tsx
@@ -75,7 +75,7 @@ const DialogContent = React.forwardRef<
         {showCloseIcon && (
           <DialogPrimitive.Close
             className={cn(
-              '~absolute ~right-4 ~top-4 ~rounded-sm ~opacity-70 ~transition-opacity hover:~opacity-100 disabled:~pointer-events-none',
+              '~absolute ~right-4 ~top-4 ~rounded-xs ~opacity-70 ~transition-opacity hover:~opacity-100 disabled:~pointer-events-none',
               '~ring-offset-background focus:~outline-none focus:~ring-2 focus:~ring-ring focus:~ring-offset-2',
               'data-[state=open]:~bg-accent data-[state=open]:~text-muted-foreground'
             )}
