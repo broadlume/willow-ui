cbc570c85a38ac85da81ba9d1fd6791c5730c763
Dhaval Javia <dhaval.javia@broadlume.com>
2025-08-18 05:12:22 +0000

    chore: btn hover cursor, bg

--- src/components/button/button-variants.ts
+++ src/components/button/button-variants.ts
@@ -3,6 +3,7 @@ import { cva } from 'class-variance-authority';
 const buttonVariants = cva(
   [
     'inline-flex h-9 items-center justify-center gap-2 overflow-hidden rounded-[100px] px-4 font-founders_grotesk text-xs font-semibold uppercase leading-none shadow',
+    'hover:cursor-pointer',
     'focus-visible:ring-ring focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-offset-2',
     'disabled:cursor-not-allowed',
   ],
@@ -10,17 +11,17 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          'text-primary-foreground bg-surface-cta text-surface-pri hover:bg-[var(--color-cta-75)] disabled:bg-[var(--color-cta-50)]',
+          'text-primary-foreground bg-surface-cta text-surface-pri hover:bg-(--color-cta-75) disabled:bg-(--color-cta-50)',
         destructive:
-          'text-primary-foreground bg-surface-destructive text-surface-pri hover:bg-[var(--color-red-400)] disabled:bg-[var(--color-red-200)]',
+          'text-primary-foreground bg-surface-destructive text-surface-pri hover:bg-(--color-red-400) disabled:bg-(--color-red-200)',
         // TODO: @dhavaljbroadlume confirm hover color with designer
         outline:
           'bg-surface-pri text-text-pri shadow-none outline outline-1 outline-offset-[-1px] outline-border-pri hover:opacity-75 disabled:opacity-50',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80 bg-[var(--color-cta-15)] text-text-cta shadow-sm outline outline-1 outline-offset-[-1px] outline-[var(--color-cta-100)] hover:opacity-75 disabled:opacity-50',
+          'bg-secondary text-secondary-foreground hover:bg-secondary/80 bg-(--color-cta-15) text-text-cta shadow-sm outline outline-1 outline-offset-[-1px] outline-(--color-cta-100) hover:opacity-75 disabled:opacity-50',
         ghost:
-          'bg-transparent shadow-none hover:bg-[var(--color-grey-10)] disabled:bg-[var(--color-grey-5)]',
-        link: 'bg-transparent text-text-cta underline-offset-4 shadow-none hover:text-[var(--color-cta-75)] hover:underline disabled:text-[var(--color-cta-50)]',
+          'bg-transparent shadow-none hover:bg-(--color-grey-10) disabled:bg-(--color-grey-5)',
+        link: 'bg-transparent text-text-cta underline-offset-4 shadow-none hover:text-(--color-cta-75) hover:underline disabled:text-(--color-cta-50)',
       },
       size: {
         default: '',
