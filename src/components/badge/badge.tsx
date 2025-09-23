04a153aa66e3241fa3c5b5660b43e1004b885022
vishalyork <146951835+vishalyork@users.noreply.github.com>
2025-09-23 09:51:44 +0530

    Badge component (#105)
    * Badge component style changes
    
    * feat: Badge varients style updated
    
    ---------
    
    Co-authored-by: pruthvish-york <pruthvish@york.ie>

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -5,7 +5,7 @@ import { cn } from '@src/lib/utils';
 
 const badgeVariants = cva(
   [
-    'tw-reset font-2xl text-2xs inline-flex cursor-default items-center rounded px-2 py-1 transition-colors gap-2 shadow',
+    'tw-reset font-medium text-xs inline-flex cursor-default items-center rounded-sm px-2 py-1 transition-colors gap-2.5',
     'focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
     'h-5',
   ],
@@ -13,18 +13,20 @@ const badgeVariants = cva(
     variants: {
       /** Variant color of the badge. */
       variant: {
-        default: 'border-transparent bg-primary text-primary-foreground shadow',
-        secondary: 'border-transparent bg-secondary text-secondary-foreground',
-        success: 'border-transparent bg-success text-success-foreground',
+        default: 'border-transparent bg-surface-cta-bg text-surface-cta',
+        secondary: 'border-transparent bg-surface-opt text-icon-pri',
+        success: 'border-transparent  bg-surface-success text-text-success',
         destructive:
-          'border-transparent bg-destructive text-destructive-foreground shadow',
-        outline: 'text-foreground',
+          'border-transparent bg-surface-destructive-bg text-text-destructive',
+        warning:
+          'border-transparent bg-surface-progress text-text-progress',
+        outline: 'border border-border-sec bg-surface-pri text-text-pri',
       },
-      /** Relative size of the badge. */
+      /** Relative size of the badge. */ 
       size: {
         default: '',
-        small: 'text-[10px]',
-        xs: 'py-0 text-[10px]',
+        small: 'text-2xs',
+        xs: 'py-0 text-2xs',
       },
     },
     defaultVariants: {
