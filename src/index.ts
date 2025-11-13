45cb3df03b57ed41e5765788a2c73ab53f88a3f5
meet.k <meet.k@york.ie>
2025-11-13 13:13:46 +0530

    add(BL-12311) - filter-panel component

--- src/index.ts
+++ src/index.ts
@@ -45,6 +45,9 @@ export * from '@components/code-editor/code-editor';
 export * from '@components/Loader/Loader';
 export * from '@components/async-autocomplete/async-autocomplete';
 export * from '@components/sidebar/sidebar';
+export * from '@components/filter-panel/filter-panel';
+export * from '@components/filter-panel/types';
+
 // Other stuff
 export * from '@src/lib/icons';
 export * from '@src/reports/summary-tile/summary-tile';
