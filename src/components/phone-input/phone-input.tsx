0a2aff077eb5be47632c997c0957772c232cb594
vijaydabhi04 <vijay.d@york.ie>
2026-02-23 11:28:56 +0530

    Enhance PhoneInput: Add custom scrollbar styles to CommandList

--- src/components/phone-input/phone-input.tsx
+++ src/components/phone-input/phone-input.tsx
@@ -255,7 +255,7 @@ const PhoneInput: React.FC<PhoneInputProps> = ({
                 onValueChange={setSearchTerm}
                 value={searchTerm}
               />
-              <CommandList className='max-h-75'>
+              <CommandList className='max-h-75 overflow-y-scroll [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 [&::-webkit-scrollbar-thumb]:rounded-md [&::-webkit-scrollbar-thumb]:hover:bg-gray-400'>
                 <CountryList
                   countries={filteredCountries}
                   showFlag={showFlag}
